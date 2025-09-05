7b8828ce349a456799cee47769817c647d970f00
Waleed Amgad <waleedbookey@gmail.com>
2025-09-05 15:58:05 +0300

    fixed missing server.cpp (main function)

---
+++ Server.cpp
@@ -0,0 +1,39 @@
+
+#include<iostream>
+#include"Node.hpp"
+#include<boost/asio.hpp>
+#include<thread>
+
+
+
+int main(int argc ,char *argv[]){
+
+    int port=0;
+
+    if(argc<2){ //no port was given
+        port=base_port;    
+        std::cerr<<"no argument was given\n";
+    }
+    else{
+       int val=atoi(argv[1]);
+       port=(short)val;
+    }
+
+    boost::asio::io_context IO_ctx;
+    std::vector<std::thread> Threads;
+
+    auto Server=std::make_shared<Node>(IO_ctx,port);
+    Server->Start_Server();
+
+    for(size_t i=0;i<2;i++){
+        Threads.emplace_back([&]{
+            IO_ctx.run();
+        });
+    }
+
+
+    for(auto& t:Threads) t.join();
+
+
+    return 0;
+}
